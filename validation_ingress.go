03a0656f5cddbee3fc82723ccae255d20b857c4d
Martin Dolozilek <martin.dolozilek@avast.com>
2018-12-17 14:50:59 +0100

    new vs. existing ingress instead of local vs. remote ingress

--- validation_ingress.go
+++ validation_ingress.go
@@ -38,49 +38,49 @@ func ValidateIngress(validation *objectValidation, ingress *extv1beta1.Ingress, 
 	if config.RuleIngressCollision {
 		targetDesc := fmt.Sprintf("Ingress %s.%s: ", ingress.Name, ingress.Namespace)
 
-		remoteIngresses, err := IngressClientAllNamespaces(clientSet).List(metav1.ListOptions{})
+		existingIngresses, err := IngressClientAllNamespaces(clientSet).List(metav1.ListOptions{})
 		if err != nil {
 			return err
 		}
-		log.Debugf("There are %d ingresses in the cluster to be compared", len(remoteIngresses.Items))
+		log.Debugf("There are %d ingresses in the cluster to be compared", len(existingIngresses.Items))
 
-		localPathData := ingressPath(ingress)
-		localTlsData := ingressTls(ingress)
+		newIngressPathData := ingressPath(ingress)
+		newIngressTlsData := ingressTls(ingress)
 
-		ValidatePathDataRegex(localPathData, validation, targetDesc)
-		ValidateTlsDataRegex(localTlsData, validation, targetDesc)
+		ValidatePathDataRegex(newIngressPathData, validation, targetDesc)
+		ValidateTlsDataRegex(newIngressTlsData, validation, targetDesc)
 
-		var remotePathData []PathDefinition
-		for _, remoteIngress := range remoteIngresses.Items {
-			remotePathData = append(remotePathData, ingressPath(&remoteIngress)...)
+		var existingIngressesPathData []PathDefinition
+		for _, existingIngress := range existingIngresses.Items {
+			existingIngressesPathData = append(existingIngressesPathData, ingressPath(&existingIngress)...)
 		}
-		ValidatePathDataCollision(localPathData, remotePathData, validation, targetDesc)
+		ValidatePathDataCollision(newIngressPathData, existingIngressesPathData, validation, targetDesc)
 
-		var remoteTlsData []TlsDefinition
-		for _, remoteIngress := range remoteIngresses.Items {
-			remoteTlsData = append(remoteTlsData, ingressTls(&remoteIngress)...)
+		var existingIngressesTlsData []TlsDefinition
+		for _, existingIngress := range existingIngresses.Items {
+			existingIngressesTlsData = append(existingIngressesTlsData, ingressTls(&existingIngress)...)
 		}
-		ValidateTlsDataCollision(localTlsData, remoteTlsData, validation, targetDesc)
+		ValidateTlsDataCollision(newIngressTlsData, existingIngressesTlsData, validation, targetDesc)
 	}
 	return nil
 }
 
-func ValidateTlsDataRegex(localTlsData []TlsDefinition, validation *objectValidation, targetDesc string) {
-	for _, localTls := range localTlsData {
-		validateHost(localTls.host, validation, targetDesc)
+func ValidateTlsDataRegex(tlsDefinition []TlsDefinition, validation *objectValidation, targetDesc string) {
+	for _, tls := range tlsDefinition {
+		validateHost(tls.host, validation, targetDesc)
 	}
 }
 
-func ValidateTlsDataCollision(localTlsData []TlsDefinition, remoteTlsData []TlsDefinition, validation *objectValidation, targetDesc string) {
-	for _, localTls := range localTlsData {
-		for _, remoteTls := range remoteTlsData {
-			if localTls.host == remoteTls.host {
+func ValidateTlsDataCollision(newIngressTlsData []TlsDefinition, existingIngressesTlsData []TlsDefinition, validation *objectValidation, targetDesc string) {
+	for _, newTls := range newIngressTlsData {
+		for _, existingTls := range existingIngressesTlsData {
+			if newTls.host == existingTls.host {
 				//if hosts are identical then also secret name and namespace has to match
-				if !(localTls.ingressNamespace == remoteTls.ingressNamespace && localTls.secretName == remoteTls.secretName) {
+				if !(newTls.ingressNamespace == existingTls.ingressNamespace && newTls.secretName == existingTls.secretName) {
 					validation.Violations.add(
 						validationViolation{
 							targetDesc,
-							fmt.Sprintf("TLS collision with '%s.%s' on '%s'", remoteTls.ingressName, remoteTls.ingressNamespace, remoteTls.host),
+							fmt.Sprintf("TLS collision with '%s.%s' on '%s'", existingTls.ingressName, existingTls.ingressNamespace, existingTls.host),
 						},
 					)
 				}
@@ -89,23 +89,23 @@ func ValidateTlsDataCollision(localTlsData []TlsDefinition, remoteTlsData []TlsD
 	}
 }
 
-func ValidatePathDataRegex(localPathData []PathDefinition, validation *objectValidation, targetDesc string) {
-	for _, localPath := range localPathData {
-		validatePath(localPath.path, validation, targetDesc)
-		validateHost(localPath.host, validation, targetDesc)
+func ValidatePathDataRegex(pathData []PathDefinition, validation *objectValidation, targetDesc string) {
+	for _, path := range pathData {
+		validatePath(path.path, validation, targetDesc)
+		validateHost(path.host, validation, targetDesc)
 	}
 }
 
-func ValidatePathDataCollision(localPathData []PathDefinition, remotePathData []PathDefinition, validation *objectValidation, targetDesc string) {
-	for _, localPath := range localPathData {
-		for _, remotePath := range remotePathData {
+func ValidatePathDataCollision(newIngressPathData []PathDefinition, existingIngressPathData []PathDefinition, validation *objectValidation, targetDesc string) {
+	for _, newIngressPath := range newIngressPathData {
+		for _, existingIngressPath := range existingIngressPathData {
 			// only other ingresses are considered - when updating it's not a collision
-			if nameWithNamespace(localPath) != nameWithNamespace(remotePath) {
-				if localPath.toUri() == remotePath.toUri() {
-					if localPath.serviceName != remotePath.serviceName || localPath.servicePort != remotePath.servicePort {
+			if nameWithNamespace(newIngressPath) != nameWithNamespace(existingIngressPath) {
+				if newIngressPath.toUri() == existingIngressPath.toUri() {
+					if newIngressPath.serviceName != existingIngressPath.serviceName || newIngressPath.servicePort != existingIngressPath.servicePort {
 						violation := validationViolation{
 							targetDesc,
-							fmt.Sprintf("Path collision with '%s' -> '%s:%s'", remotePath.toUri(), remotePath.serviceName, remotePath.servicePort),
+							fmt.Sprintf("Path collision with '%s' -> '%s:%s'", existingIngressPath.toUri(), existingIngressPath.serviceName, existingIngressPath.servicePort),
 						}
 						validation.Violations.add(violation)
 					}
